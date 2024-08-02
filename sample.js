eb29819d2ba470fdceada06b6bff61373ae2d6b5
Teja <tejasimma033@gmail.com>
2024-08-02 22:51:23 +0530

    02/08/2024 class

---
+++ sample.js
@@ -0,0 +1,11 @@
+document.getElementById("anil").innerHTML="<h1>teja</h1>"
+
+document.getElementsByClassName("anil")[1].textContent="<h1>teja</h1>"
+
+document.getElementsByTagName
+document.querySelector("#anil")
+document.querySelectorAll(".anil")
+
+const con = document.createElement('names');
+con.innerHTML="srkr"
+document.body.appendChild(con);
