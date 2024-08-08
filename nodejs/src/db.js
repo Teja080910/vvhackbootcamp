bf2ee059e9771876ca32ffe0f2e9ebd94d960752
Teja <tejasimma033@gmail.com>
2024-08-08 21:54:48 +0530

    08/08/2024 session

--- nodejs/src/db.js
+++ nodejs/src/db.js
@@ -5,7 +5,7 @@ async function connectToDB(cb) {
     const url = "mongodb://localhost:27017"
     const client = new MongoClient(url);
     await client.connect();
-    db = client.db("AST_Database");
+    db = client.db("vvhack");
     cb();
 }
 
--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -1,18 +1,94 @@
 import cors from 'cors';
 import express from 'express';
-import { connectToDB } from "./db.js";
+import { connectToDB, db } from "./db.js";
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
     res.json("server is running successfully!");
 })
 
-app.post('/ast', (req, res) => {
-    console.log({Name:req.body.name,AST:req.body.team})
-    res.json({Name:req.body.name,AST:req.body.team});
+app.post('/ast', async(req, res) => {
+    await db.collection("ast").find().toArray()
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+
+app.post('/insert', async(req, res) => {
+    
+    await db.collection("ast").insertOne({Name:req.body.name,Team:req.body.team})
+    .then((result)=>{
+        res.json(result)
+    }).catch((e)=>console.log(e))
+ 
+})
+
+app.post('/insertmany', async(req, res) => {
+    await db.collection("ast").insertMany(req.body)
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+
+app.post('/findone', async(req, res) => {
+    await db.collection("ast").findOne({Name:"teja"})
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+
+app.post('/findmany', async(req, res) => {
+    await db.collection("ast").find().toArray()
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+
+app.post('/updateone', async(req, res) => {
+    await db.collection("ast").updateOne({Name:"teja"},{$set:{Age:20}})
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+
+app.post('/updateone', async(req, res) => {
+    await db.collection("ast").findOneAndUpdate({Name:"teja"},{$set:{Age:20}})
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+
+app.post('/updatemany', async(req, res) => {
+    await db.collection("ast").updateMany({Age:20},{$set:{Age:25}})
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+
+app.post('/deleteone', async(req, res) => {
+    await db.collection("ast").deleteOne({Name:"teja"})
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+
+app.post('/detemany', async(req, res) => {
+    await db.collection("ast").deleteMany()
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
 })
 
 
